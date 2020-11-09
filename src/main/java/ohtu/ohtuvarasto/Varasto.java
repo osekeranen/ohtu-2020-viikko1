774a7515555c5e36c7a0e9da18930f879fb3bb71
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-11-09 21:37:47 +0200

    fix error

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -71,8 +71,7 @@ public class Varasto {
     }
 
     // --- Merkkijonoesitys Varasto-oliolle: ----
-    public String toString() 
-    {
+    public String toString() {
         return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
     }
 }
